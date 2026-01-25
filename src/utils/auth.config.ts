dde5703ca3e3335f75cb539498be10ea2ab8301e
Pikacnu <a13456123456@gmail.com>
2026-01-25 14:36:33 +0800

    chore: update dependencies in bun.lock and format auth.config.ts for improved readability

--- src/utils/auth.config.ts
+++ src/utils/auth.config.ts
@@ -3,25 +3,25 @@ import Discord from 'next-auth/providers/discord';
 import Google from 'next-auth/providers/google';
 
 export default {
-	providers: [
-		Discord({
-			clientId: process.env.AUTH_DISCORD_ID || '',
-			clientSecret: process.env.AUTH_DISCORD_SECRET || '',
-		}),
-		Google({
-			clientId: process.env.AUTH_GOOGLE_ID || '',
-			clientSecret: process.env.AUTH_GOOGLE_SECRET || '',
-		}),
-	],
-	callbacks: {
-		authorized({ request, auth }) {
-			const url = new URL(request.url);
-			if (url.pathname.startsWith('/dashboard')) {
-				if (auth) return true;
-				const url = new URL('/auth/login', request.url);
-				return Response.redirect(url.toString(), 302);
-			}
-			return true;
-		},
-	},
+  providers: [
+    Discord({
+      clientId: process.env.AUTH_DISCORD_ID || '',
+      clientSecret: process.env.AUTH_DISCORD_SECRET || '',
+    }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID || '',
+      clientSecret: process.env.AUTH_GOOGLE_SECRET || '',
+    }),
+  ],
+  callbacks: {
+    authorized({ request, auth }) {
+      const url = new URL(request.url);
+      if (url.pathname.startsWith('/dashboard')) {
+        if (auth) return true;
+        const url = new URL('/auth/login', request.url);
+        return Response.redirect(url.toString(), 302);
+      }
+      return true;
+    },
+  },
 } satisfies NextAuthConfig;
